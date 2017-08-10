3dab9927a400ae6c35ed7bd457a661f35ff688e6
Sokos6 <will.sokolowski@gmail.com>
2017-08-10 17:20:43 -0400

    product template

--- app/products/product.component.ts
+++ app/products/product.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
+import { Product } from './product';
 
 @Component({
     moduleId: module.id,
     templateUrl: 'product.template.html'
 })
 
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+	products: Product[];
+
+	constructor(private _productService: ProductService) {}
+
+	ngOnInit() {
+		
+	}
 
 }
